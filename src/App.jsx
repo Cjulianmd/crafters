6346446e3b3173462e4b49a1cc37a3ba28552fb5
cmjuliand <julian.gallo.v@gmail.com>
2023-09-09 13:12:25 -0500

    firt commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Hperfil from '../src/components/Hperfil'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Hperfil/>
     </>
   );
 }
