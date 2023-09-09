e2a7f212d41ce5bb5289738f8957c65ab95714bd
Ana Rojas <anac.rojas88@gmail.com>
2023-09-09 16:17:27 -0500

    form login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import RegisterUser from "./registerUser/RegisterUser";
 import Home from "./views/Home";
-
+import Login from "./registerUser/Login";
 function App() {
   return (
     <main>
-    <RegisterUser />
+    {/* <RegisterUser /> */}
+    <Login />
     </main>
   );
 }
